75df592ca35538613a6118189be797c56d909f31
mohammedsalmannn <rmdsalman001@gmail.com>
2021-07-17 15:07:13 +0530

    Added : table data Local Storage added to the Form

--- js/home.js
+++ js/home.js
@@ -1,8 +1,17 @@
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', () => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 });
 
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 const createInnerHtml = () => {
+
+    if (employeePayrollList.length == 0) return;
+
     const headerHtml = `<tr>
     <th></th>
     <th>Name</th>
@@ -12,22 +21,22 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Actions</th>
     </tr>`;
+
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJson();
     for (const employeePayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
-        <tr>        
-        <td><img class="profile" alt="" src="${employeePayrollData._profilePic}"></td>
-        <td>${employeePayrollData._name}</td>
-        <td>${employeePayrollData._gender}</td>
-        <td>${getDeptHtml(employeePayrollData._department)}</td>
-        <td>${employeePayrollData._salary}</td>
-        <td>${employeePayrollData._startDate}</td>
-        <td>
-        <img name="${employeePayrollData._id}" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-        <img name="${employeePayrollData._id}" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
-    </td>
-</tr>`;
+        <tr>
+            <td><img class="profile" alt="" src="${employeePayrollData._profilePic}"></td>
+            <td>${employeePayrollData._name}</td>
+            <td>${employeePayrollData._gender}</td>
+            <td>${getDeptHtml(employeePayrollData._department)}</td>
+            <td>${employeePayrollData._salary}</td>
+            <td>${formatDate(employeePayrollData._startDate)}</td>
+            <td>
+                <img name="${employeePayrollData._id}" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img name="${employeePayrollData._id}" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+    </tr>`;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
@@ -40,34 +49,11 @@ const getDeptHtml = (deptList) => {
     return deptHtml;
 }
 
-const createEmployeePayrollJson = () => {
-    let employeePayrollListLocal = [
-        {
-            _name: 'Gopi Pawar',
-            _gender: 'Female',
-            _department: [
-                'HR',
-                'Sales'
-            ],
-            _salary: '350000',
-            _startDate: '29 Oct 2020',
-            _note: 'This is Gopi',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -1.png'
-        },
-        {
-            _name: 'Rahul Patil',
-            _gender: 'Male',
-            _department: [
-                'Engineering',
-            ],
-            _salary: '400000',
-            _startDate: '9 Nov 2020',
-            _note: 'This is Rahul',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -3.png'
-        }
-    ];
-
-    return employeePayrollListLocal;
+const formatDate = (date) => {
+    let startDate = new Date(date);
+    const options = {
+        year: 'numeric', month: 'long', day: 'numeric'
+    };
+    const empDate = !startDate ? "undefined" : startDate.toLocaleDateString("en-IN", options);
+    return empDate;
 }
